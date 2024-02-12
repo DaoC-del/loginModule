b276eb24de88eab18328f178b9c840526472542e
ヨ <550164003@163.com>
2024-02-12 23:21:56 +0900

    docker db service

--- src/main/java/com/server/loginmodule/LoginModuleApplication.java
+++ src/main/java/com/server/loginmodule/LoginModuleApplication.java
@@ -1,8 +1,9 @@
 package com.server.loginmodule;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.server.loginmodule.mapper")
 @SpringBootApplication
 public class LoginModuleApplication {
 
--- src/main/java/com/server/loginmodule/mapper/UserMapper.java
+++ src/main/java/com/server/loginmodule/mapper/UserMapper.java
@@ -2,11 +2,7 @@ package com.server.loginmodule.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.server.loginmodule.model.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
-@Mapper
 public interface UserMapper extends BaseMapper<User> {
-    @Select("SELECT * FROM user WHERE username = #{username}")
-    User selectByUsername(String username);
+    // 这里可以添加一些自定义的方法，如果需要
 }
