f33203e1967026a5636aeca87486d25c49b895e5
pc011 <550164003@qq.com>
2024-02-15 15:09:06 +0900

    セキュリティフィルターチェンの実装①

--- src/main/java/com/server/loginmodule/config/SecurityConfig.java
+++ src/main/java/com/server/loginmodule/config/SecurityConfig.java
@@ -1,14 +1,39 @@
 package com.server.loginmodule.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    // 配置PasswordEncoder bean
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    // 禁用Spring Security的拦截器
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authz -> authz
+                        .requestMatchers("/auth/**").permitAll() // 允许/auth/*下的所有请求
+                        .anyRequest().authenticated() // 其他所有请求都需要认证
+                );
+              // 禁用CSRF保护，对于REST API通常不需要
+
+        // 其他配置...
+
+        return http.build();
+    }
+
+
+
 }
